cd6c7b1778425100626a562c2bc0af0309294ea3
Javeria Khan <asadkhan42360@gmail.com>
2023-08-20 20:05:13 +0500

    my github

---
+++ code5.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+int main(){
+  /*  int a=4;
+    int b=6;
+    
+    // Bitwise AND/OR/NOT/XOR Operators
+    cout<<"a&b "<<(a&b)<<endl;
+    cout<<"a|b "<<(a|b)<<endl;
+    cout<<"~a "<<~a<<endl;
+    cout<<"a^b "<<(a^b)<<endl;
+
+    // left shift operator/right shift operator
+   cout<< (15>>1) <<endl;
+   cout<< (20>>2) <<endl;
+   cout<< (19<<1) <<endl;
+   cout<< (21<<2) <<endl;
+   
+  //  Pre/Post Increment/Decrement Operator
+  int i=8;
+  // Post Increment
+   cout<<(i++) <<endl;
+  // Pre Increment
+   cout<<(++i) <<endl;
+  // Post Decrement
+   cout<<(i--) <<endl;
+  // Pre Decrement
+   cout<<(--i) <<endl;
+   
+  //  Output Questons
+  // Q no 1
+  int a,b=1;
+  a=10;
+  if(++a){
+    cout<<b;
+  }
+  else{
+    cout<<++b;
+  }
+  
+  // Q no 2
+ int a=1;
+ int b=2;
+ if(a--> 0 && ++b> 2){
+  cout<<"stage 1-Inside If ";
+ }
+ else{
+  cout<<"stage 2-Inside else ";
+ }
+ cout<< a <<" "<< b <<endl;
+
+ // Q no 3
+ int number=3;
+ cout<<(25*(++number));
+ 
+ // Q no 4
+ int a=1;
+ int b=a++;
+ int c=++a;
+ cout<<b;
+ cout<<c;
+
+ // For Loop
+ int n;
+ cout<<"Enter the value of n  ";
+ cin>> n;
+ cout<<"Print counting from 1 to n" <<endl;
+ for(int i=1;i<=n;i++){
+  cout<<i<<endl;
+ }
+
+ int n;
+ cout<<"Enter the value of n  ";
+ cin>> n;
+ cout<<"Print counting from 1 to n" <<endl;
+ int i=1;
+ for(;;){
+  if(i<=n){
+  cout<<i<<endl;
+  }
+  else{
+    break;
+  }
+ i++;
+ }
+ 
+  
+  int n;
+  cout<<"Enter the value of n ";
+  cin>> n;
+  int sum=0;
+  for(int i=1;i<=n;i++){
+    sum+=i;
+  }
+  cout<<sum<<endl;
+  *
+ int n=10;
+ int a=0;
+ int b=1;
+ cout<<a<<endl<<b<<endl;
+ for(int i=1;i<=n;i++){
+  int c=a+b;
+  cout<<c<<endl;
+  a=b;
+  b=c;
+ }
+*/
+}
